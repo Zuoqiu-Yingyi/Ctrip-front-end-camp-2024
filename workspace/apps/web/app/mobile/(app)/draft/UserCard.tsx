6b9140b394abcac002ef5acb47fa8454092c5bd5
tuoniaodan <105632392+tuoniaodan@users.noreply.github.com>
2024-04-12 22:26:00 +0800

    feat(mobile) 修改草稿页 | fix draft

--- workspace/apps/web/app/mobile/(app)/draft/UserCard.tsx
+++ workspace/apps/web/app/mobile/(app)/draft/UserCard.tsx
@@ -27,58 +27,21 @@ import { StoreContext } from "@/providers/store";
 
 const UserCard = ({ key, thisUid, coverUid, title, content, onClick }: { key: string; thisUid: string; coverUid: string; title: string; content: string; onClick: (uid: string) => void }) => {
     const { t, i18n } = useTranslation();
-    const { trpc } = useContext(StoreContext);
-    const response = trpc.review.submitted.query({
-        draft_id: thisUid,
-    });
-    response_content = response.data?.reviews[0]?.content;
 
     return (
         <div className={styles.usercard}>
-            <div className={styles.draftcontainer}>
-                <div className={styles.imagecontainer}>
-                    <Image
-                        src={coverUid}
-                        loader={assetsLoader}
-                        alt={t("cover")}
-                        className={styles.image}
-                        fill={true}
-                    />
-                </div>
-                <div className={styles.wordcontainer}>
-                    <p className={styles.cardtitle}>{title}</p>
-                    <p className={styles.cardcontent}>{content}</p>
-                </div>
+            <div className={styles.imagecontainer}>
+                <Image
+                    src={coverUid}
+                    loader={assetsLoader}
+                    alt={t("cover")}
+                    className={styles.image}
+                    fill={true}
+                />
             </div>
-
-            <div className={styles.buttoncontainer}>
-                <Button
-                    color="primary"
-                    fill="solid"
-                    className={styles.button}
-                    onClick={() => {
-                        Modal.alert({
-                            content: { response_content },
-                            closeOnMaskClick: true,
-                        });
-                    }}
-                >
-                    {t("state")}
-                </Button>
-                <Button
-                    color="primary"
-                    fill="solid"
-                    className={styles.button}
-                >
-                    {t("delete")}
-                </Button>
-                <Button
-                    color="primary"
-                    fill="solid"
-                    className={styles.button}
-                >
-                    {t("edit")}
-                </Button>
+            <div className={styles.wordcontainer}>
+                <p className={styles.cardtitle}>{title}</p>
+                <p className={styles.cardcontent}>{content}</p>
             </div>
         </div>
     );
