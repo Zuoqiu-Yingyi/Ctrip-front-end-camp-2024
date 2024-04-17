4a49d197933059b379cf8c9dcb09667865072274
wu <3025384698@qq.com>
2024-04-17 14:55:47 +0800

    feat(mobile): 用户头像上传 |  User profile upload

--- workspace/apps/web/app/mobile/(app)/user/page.tsx
+++ workspace/apps/web/app/mobile/(app)/user/page.tsx
@@ -31,6 +31,8 @@ import {
     ActionSheet,
     Toast,
     Picker,
+    Mask,
+    Image
 } from "antd-mobile";
 import type { Action } from "antd-mobile/es/components/action-sheet";
 
@@ -46,6 +48,7 @@ import {
 } from "@/utils/message";
 import { Locale } from "@/utils/locale";
 import { Theme } from "@/utils/theme";
+import AvaterEditor from "@/ui/AvaterEditor";
 import DeleteAccountPopup from "./DeleteAccountPopup";
 
 export default function InfoPage(): JSX.Element {
@@ -194,7 +197,9 @@ export default function InfoPage(): JSX.Element {
 
     return (
         <>
-            <NavBar backArrow={false}>{t("me")}</NavBar>
+            <NavBar backArrow={false}>{t("me")}</NavBar>   
+
+            <AvaterEditor avatar={'https://images.unsplash.com/photo-1567945716310-4745a6b7844b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=60'}/>        
 
             {/* REF: https://mobile.ant.design/zh/components/list */}
             <List>
@@ -263,7 +268,7 @@ export default function InfoPage(): JSX.Element {
                         visible={changePasswordPopupVisible}
                         onSuccess={clearUserData}
                         onClose={() => setChangePasswordPopupVisible(false)}
-                        />
+                    />
 
                     {/* 删除账户 */}
                     <DeleteAccountPopup
